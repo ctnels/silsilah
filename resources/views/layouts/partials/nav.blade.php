426560c921343b047195702011a25ec056acc3a9
Chuks Nwaneri <ctnels@gmail.com>
2024-06-03 23:17:29 +0100

    Remove lang menu, birthday and search menu

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,7 +3,10 @@
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+            <button type="button"
+                class="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -11,35 +14,42 @@
             </button>
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand"
+                href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
         </div>
 
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <div class="collapse navbar-collapse"
+            id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li><a href="{{ route('users.search') }}">{{ __('app.search_your_family') }}</a></li>
-                <li><a href="{{ route('birthdays.index') }}">{{ __('birthday.birthday') }}</a></li>
+                {{-- <li><a href="{{ route('users.search') }}">{{ __('app.search_your_family') }}</a></li>
+                <li><a href="{{ route('birthdays.index') }}">{{ __('birthday.birthday') }}</a></li> --}}
             </ul>
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                <?php $mark = (preg_match('/\?/', url()->current())) ? '&' : '?';?>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">en</a></li>
+                <?php $mark = preg_match('/\?/', url()->current()) ? '&' : '?'; ?>
+                {{-- <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">en</a></li>
                 <li><a href="{{ url(url()->current() . $mark . 'lang=id') }}">id</a></li>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=ur') }}">ur</a></li>
+                <li><a href="{{ url(url()->current() . $mark . 'lang=ur') }}">ur</a></li> --}}
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
                 @else
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <a href="#"
+                            class="dropdown-toggle"
+                            data-toggle="dropdown"
+                            role="button"
+                            aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
-                        <ul class="dropdown-menu" role="menu">
+                        <ul class="dropdown-menu"
+                            role="menu">
                             @if (is_system_admin(auth()->user()))
                                 <li><a href="{{ route('backups.index') }}">{{ __('backup.list') }}</a></li>
                             @endif
@@ -52,7 +62,10 @@
                                     Logout
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form"
+                                    action="{{ route('logout') }}"
+                                    method="POST"
+                                    style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
